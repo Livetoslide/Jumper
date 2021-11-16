687a49098ac88fe15815c5a8d8ed226d89b8bb64
Zimarev_Alexandr <Zimarev_Alexandr@SCRIPT17>
2021-11-16 15:36:51 +0300

    Добавьте файлы проекта.

---
+++ Jumper.cpp
@@ -0,0 +1,95 @@
+﻿// Jumper.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <time.h>
+using namespace sf;
+
+struct point
+{
+    int x, y;
+};
+
+int main()
+{
+    srand(time(0));
+
+    RenderWindow app(VideoMode(400, 533), "Game");
+    app.setFramerateLimit(60);
+    Texture t1, t2, t3;
+    t1.loadFromFile("images/background.png");
+    t2.loadFromFile("images/platform.png");
+    t3.loadFromFile("images/doodle.png");
+
+    Sprite sBackground(t1), sPlat(t2), sPers(t3);
+
+    point plat[20];
+
+    for (int i = 0; i < 10; i++)
+    {
+        plat[i].x = rand() % 400;
+        plat[i].y = rand() % 533;
+    }
+    int x = 100, y = 100, h = 200;
+    float dx = 0, dy = 0;
+
+    while (app.isOpen())
+    {
+        Event e;
+        while (app.pollEvent(e))
+        {
+            if (e.type == Event::Closed)
+                app.close();
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::Right)) x += 3;
+        if (Keyboard::isKeyPressed(Keyboard::Left)) x -= 3;
+
+        dy += 0.2;
+        y += dy;
+        if (y > 500) dy = -10;
+
+
+        if (y<h)
+            for (int i = 0; i < 10; i++)
+            {
+                y = h;
+                plat[i].y = plat[i].y - dy;
+                if (plat[i].y > 533) { plat[i].y = 0; plat[i].x = rand() % 400; }
+            }
+
+        for (int i = 0; i < 10; i++)
+        {
+            if ((x + 50 > plat[i].x) && (x + 20 < plat[i].x + 68)
+                && (y + 70 > plat[i].y) && (y + 70 < plat[i].y + 14) && (dy > 0)) dy -= 14;
+        }
+        
+
+        sPers.setPosition(x, y);
+
+
+        app.draw(sBackground);
+        app.draw(sPers);
+        for (int i = 0; i < 10; i++)
+        {
+            sPlat.setPosition(plat[i].x, plat[i].y);
+            app.draw(sPlat);
+        }
+
+        app.display();
+    }
+
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
